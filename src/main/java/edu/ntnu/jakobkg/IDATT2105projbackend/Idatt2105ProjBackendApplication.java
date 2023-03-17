ef4d7d365b02bd152f2082ee2e637fb27f26ad53
Jakob Grønhaug <jakob@gronha.ug>
2023-03-17 15:54:50 +0100

    🎉 Initial commit

---
+++ src/main/java/edu/ntnu/jakobkg/IDATT2105projbackend/Idatt2105ProjBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.ntnu.jakobkg.IDATT2105projbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Idatt2105ProjBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Idatt2105ProjBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ntnu/jakobkg/IDATT2105projbackend/Idatt2105ProjBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ntnu.jakobkg.IDATT2105projbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Idatt2105ProjBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
