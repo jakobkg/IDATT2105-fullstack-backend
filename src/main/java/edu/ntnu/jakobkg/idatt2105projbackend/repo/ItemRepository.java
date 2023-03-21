117ac712e48a3351698f20c3879f3b534616ad12
Kasia <kasia.szlejter@gmail.com>
2023-03-21 13:54:52 +0100

    Changed from CrudRepository to JpaRepository for paging support

--- src/main/java/edu/ntnu/jakobkg/idatt2105projbackend/repo/ItemRepository.java
+++ src/main/java/edu/ntnu/jakobkg/idatt2105projbackend/repo/ItemRepository.java
@@ -1,6 +1,6 @@
 package edu.ntnu.jakobkg.idatt2105projbackend.repo;
 
 import edu.ntnu.jakobkg.idatt2105projbackend.model.Item;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemRepository extends CrudRepository<Item, Integer> {}
+public interface ItemRepository extends JpaRepository<Item, Integer> {}
