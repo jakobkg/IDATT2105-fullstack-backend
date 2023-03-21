bd167a1f9c12ab057c076f624f75f6014ae41198
Jakob Grønhaug <jakob@gronha.ug>
2023-03-21 21:34:18 +0100

    Autentisering implementert

--- src/main/java/edu/ntnu/jakobkg/idatt2105projbackend/Security/AuthFilter.java
+++ src/main/java/edu/ntnu/jakobkg/idatt2105projbackend/Security/AuthFilter.java
@@ -1,21 +1,77 @@
 package edu.ntnu.jakobkg.idatt2105projbackend.Security;
 
-import java.io.IOException;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
-import org.springframework.web.filter.OncePerRequestFilter;
-
+import edu.ntnu.jakobkg.idatt2105projbackend.controller.TokenController;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+import java.util.Collections;
+
+@Controller
 public class AuthFilter extends OncePerRequestFilter {
 
+    private static final Logger LOGGER = LogManager.getLogger(AuthFilter.class);
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        // check Bearer auth header
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // if Bearer auth header exists, validate token, and extract userId from token.
+        // Note that we have added userId as subject to the token when it is generated
+        // Note also that the token comes in this format 'Bearer token'
+        String token = header.substring(7);
+        final String username = validateTokenAndGetUserId(token);
+        if (username == null) {
+            // validation failed or token expired
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // if token is valid, add user details to the authentication context
+        // Note that user details should be fetched from the database in real scenarios
+        // this is case we will retrieve use details from mock
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                username,
+                null,
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        // then, continue with authenticated user context
         filterChain.doFilter(request, response);
-        return;
     }
-    
+
+    public String validateTokenAndGetUserId(final String token) {
+        try {
+            final Algorithm hmac512 = Algorithm.HMAC512(TokenController.secret);;
+            final JWTVerifier verifier = JWT.require(hmac512).build();
+            return verifier.verify(token).getSubject();
+        } catch (final JWTVerificationException verificationEx) {
+            LOGGER.warn("token is invalid: {}", verificationEx.getMessage());
+            return null;
+        }
+    }
 }
--- src/main/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/UserController.java
+++ src/main/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/UserController.java
@@ -4,23 +4,28 @@ import java.util.List;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+import edu.ntnu.jakobkg.idatt2105projbackend.helper.TokenHelper;
 import edu.ntnu.jakobkg.idatt2105projbackend.model.AddUserRequest;
-import edu.ntnu.jakobkg.idatt2105projbackend.model.UserModel;
+import edu.ntnu.jakobkg.idatt2105projbackend.model.User;
+import edu.ntnu.jakobkg.idatt2105projbackend.model.User.UserType;
 import edu.ntnu.jakobkg.idatt2105projbackend.repo.UserRepository;
 
 @Controller
@@ -31,10 +36,14 @@ public class UserController {
 
     @PostMapping(path = "")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public @ResponseBody String addUser(
+    public @ResponseBody void addUser(
             @RequestBody AddUserRequest request) {
 
-        UserModel newUser = new UserModel(
+        if (userRepo.existsByEmail(request.email())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        }
+
+        User newUser = new User(
                 request.firstname(),
                 request.lastname(),
                 request.email(),
@@ -44,52 +53,66 @@ public class UserController {
                 request.city());
 
         userRepo.save(newUser);
-        return "OK";
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public void deleteUser(@RequestHeader("Authorization") String token, @PathVariable Integer id) {
+        String authenticatedUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+        User loggedinUser = userRepo.findByEmail(authenticatedUsername).orElseThrow();
+
+        if (loggedinUser.getType() == UserType.ADMIN || loggedinUser.getId() == id) {
+            userRepo.deleteById(id);
+        } else {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
     }
 
     @GetMapping(path = "/{id}")
     @ResponseStatus(code = HttpStatus.OK)
-    public @ResponseBody UserModel getUser(@PathVariable Integer id) {
+    public @ResponseBody User getUser(@PathVariable Integer id) {
         return userRepo.findById(id).orElseThrow();
     }
 
     @PutMapping(path = "/{id}")
     @ResponseStatus(code = HttpStatus.OK)
     public @ResponseBody String updateUser(@PathVariable Integer id,
-            @RequestParam(required = false) String firstname,
-            @RequestParam(required = false) String lastname,
-            @RequestParam(required = false) String email,
-            @RequestParam(required = false) String password,
-            @RequestParam(required = false) String streetAddress,
-            @RequestParam(required = false) Integer postCode,
-            @RequestParam(required = false) String city) {
+            @RequestBody AddUserRequest request) {
     
-        UserModel user = userRepo.findById(id).orElseThrow(() -> {
+        User user = userRepo.findById(id).orElseThrow(() -> {
             return new ResponseStatusException(HttpStatus.NOT_FOUND);
         });
 
-        if (Objects.nonNull(firstname)) {
-            user.setFirstname(firstname);
+        if (userRepo.existsByEmail(request.email())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
         }
 
-        if (Objects.nonNull(lastname)) {
-            user.setLastname(lastname);
+        if (Objects.nonNull(request.firstname())) {
+            user.setFirstname(request.firstname());
         }
 
-        if (Objects.nonNull(email)) {
-            user.setEmail(email);
+        if (Objects.nonNull(request.lastname())) {
+            user.setLastname(request.lastname());
         }
 
-        if (Objects.nonNull(streetAddress)) {
-            user.setStreetAddress(streetAddress);
+        if (Objects.nonNull(request.email())) {
+            user.setEmail(request.email());
         }
 
-        if (Objects.nonNull(postCode)) {
-            user.setPostCode(postCode);
+        if (Objects.nonNull(request.password())) {
+            user.setPassword(request.password());
         }
 
-        if (Objects.nonNull(city)) {
-            user.setCity(city);
+        if (Objects.nonNull(request.streetAddress())) {
+            user.setStreetAddress(request.streetAddress());
+        }
+
+        if (Objects.nonNull(request.postCode())) {
+            user.setPostCode(request.postCode());
+        }
+
+        if (Objects.nonNull(request.city())) {
+            user.setCity(request.city());
         }
 
         userRepo.save(user);
@@ -97,7 +120,7 @@ public class UserController {
     }
 
     @GetMapping(path = "")
-    public @ResponseBody List<UserModel> getAllUsers(
+    public @ResponseBody List<User> getAllUsers(
             @RequestParam(defaultValue = "0") Integer page,
             @RequestParam(defaultValue = "24") Integer perPage) {
         return userRepo.findAll(PageRequest.of(page, perPage)).toList();
