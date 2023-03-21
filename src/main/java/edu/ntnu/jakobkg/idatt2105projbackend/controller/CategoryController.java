026672f1da834c545a0411c031cf1cd5a6c06e49
ingrid <ingridmegge@gmail.com>
2023-03-21 12:44:00 +0100

    oppdaterte controllermetoder

--- src/main/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/CategoryController.java
+++ src/main/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/CategoryController.java
@@ -1,35 +1,45 @@
 package edu.ntnu.jakobkg.idatt2105projbackend.controller;
-
 import edu.ntnu.jakobkg.idatt2105projbackend.model.Category;
 import edu.ntnu.jakobkg.idatt2105projbackend.repo.CategoryRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
+@RequestMapping("/category")
 public class CategoryController {
-
     private final CategoryRepository categoryRepository;
 
     CategoryController(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
 
-    @PostMapping("/category")
+    @PostMapping("")
     Category createNewCategory( Category newCategory){
         return categoryRepository.save(newCategory);
     }
 
-    @GetMapping("/category")
+    @GetMapping("")
     Iterable<Category> all() {
         return categoryRepository.findAll();
     }
 
-    @GetMapping("/category/{id}")
-    Optional<Category> getCategory(@PathVariable Integer id){
-        return categoryRepository.findById(id);
+    @PutMapping("/{id}")
+    Optional<Category> updateCategoryName(@RequestBody Category updatedCategory,@PathVariable Integer id){
+        return categoryRepository.findById(id).map(
+                category-> {
+                    category.setCategoryName(updatedCategory.getCategoryName());
+                    return categoryRepository.save(category);
+                });
+        }
+
+    @GetMapping("/{id}")
+    Category getCategory(@PathVariable Integer id){
+        Optional<Category> result = categoryRepository.findById(id);
+        return result.orElseThrow(() -> new NoSuchElementException(String.valueOf(id)));
     }
 
-    @DeleteMapping("/category/{id}")
+    @DeleteMapping("/{id}")
     void deleteCategory(@PathVariable Integer id){
         categoryRepository.deleteById(id);
     }
