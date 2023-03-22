9eb62e8a6bb644879f9f56de65bc54a372d8cc04
Sondre Malerud <sondrmal@stud.ntnu.no>
2023-03-22 11:51:49 +0100

    Fikset så egendefinerte HTTP-statuser funker i kasting av unntak

--- src/main/java/edu/ntnu/jakobkg/idatt2105projbackend/idatt2105ProjBackendApplication.java
+++ src/main/java/edu/ntnu/jakobkg/idatt2105projbackend/idatt2105ProjBackendApplication.java
@@ -1,11 +1,14 @@
 package edu.ntnu.jakobkg.idatt2105projbackend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class idatt2105ProjBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(idatt2105ProjBackendApplication.class, args);
 	}
