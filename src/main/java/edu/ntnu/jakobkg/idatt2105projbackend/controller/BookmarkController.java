10b66f3c3099c2738c92f8936e09c130277bd42f
Sondre Malerud <sondrmal@stud.ntnu.no>
2023-03-22 11:52:21 +0100

    Autentisering skal funke

--- src/main/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/BookmarkController.java
+++ src/main/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/BookmarkController.java
@@ -2,22 +2,25 @@ package edu.ntnu.jakobkg.idatt2105projbackend.controller;
 
 import edu.ntnu.jakobkg.idatt2105projbackend.model.Bookmark;
 import edu.ntnu.jakobkg.idatt2105projbackend.model.BookmarkId;
+import edu.ntnu.jakobkg.idatt2105projbackend.model.User;
 import edu.ntnu.jakobkg.idatt2105projbackend.repo.BookmarkRepository;
+import edu.ntnu.jakobkg.idatt2105projbackend.repo.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.awt.print.Book;
 import java.util.List;
 
 @RestController
 @RequestMapping("/bookmark")
 public class BookmarkController {
     Logger logger = LoggerFactory.getLogger(BookmarkController.class);
+
+    @Autowired
+    private UserRepository userRepo;
 
     @Autowired
     BookmarkRepository bookmarkRepository;
@@ -28,9 +31,13 @@ public class BookmarkController {
 
     @PostMapping(path = "/{itemId}")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public @ResponseBody Bookmark addBookmark(@PathVariable int itemId) {
-        //todo: her henter vi userId via jwt token
-        int userId = 1;
+    public @ResponseBody Bookmark addBookmark(@RequestHeader("Authorization") String token, @PathVariable int itemId) {
+
+        // Authentication
+        String authenticatedUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+        User loggedinUser = userRepo.findByEmail(authenticatedUsername).orElseThrow();
+        int userId = loggedinUser.getId();
+
         //todo: fjern kommentering når ItemRepository finnes
 /*        if (!itemRepository.existsById(itemId)) {
             logger.warn("Item with itemId: " + itemId + " does not exist");
@@ -44,9 +51,11 @@ public class BookmarkController {
 
     @DeleteMapping(path = "/{itemId}")
     @ResponseStatus(code = HttpStatus.OK)
-    public @ResponseBody void deleteBookmark(@PathVariable int itemId) {
-        //todo: her henter vi userId via jwt token
-        int userId = 1;
+    public @ResponseBody void deleteBookmark(@RequestHeader("Authorization") String token, @PathVariable int itemId) {
+        // Authentication
+        String authenticatedUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+        User loggedinUser = userRepo.findByEmail(authenticatedUsername).orElseThrow();
+        int userId = loggedinUser.getId();
 
         // HTTP status 200 if there is nothing to delete
         if (!bookmarkRepository.existsById(new BookmarkId(userId, itemId))) return;
@@ -55,9 +64,12 @@ public class BookmarkController {
     }
 
     @GetMapping(path = "/{itemId}")
-    public @ResponseBody Bookmark isBookmarked(@PathVariable int itemId) {
-        //todo: her henter vi userId via jwt token
-        int userId = 1;
+    public @ResponseBody Bookmark isBookmarked(@RequestHeader("Authorization") String token, @PathVariable int itemId) {
+        // Authentication
+        String authenticatedUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+        User loggedinUser = userRepo.findByEmail(authenticatedUsername).orElseThrow();
+        int userId = loggedinUser.getId();
+
         Bookmark bookmark = bookmarkRepository.findById(new BookmarkId(userId, itemId)).orElseThrow(() -> {
             logger.warn("Bookmark with userId: " + userId + " and itemId: " + itemId + " not found.");
             return new ResponseStatusException(HttpStatus.NOT_FOUND);
@@ -68,13 +80,14 @@ public class BookmarkController {
 
 
     @GetMapping(path = "")
-    public @ResponseBody List<Bookmark> getAllBookmarksByUserId() {
-        //todo: her henter vi userId via jwt token
-        int userId = 1;
-        List<Bookmark> result = bookmarkRepository.findByBookmarkIdUserId(userId);
-        // returns HTTP status 200 if user has no bookmarks
-        if (result.isEmpty()) throw new ResponseStatusException(HttpStatus.OK);
+    public @ResponseBody List<Bookmark> getAllBookmarksByUserId(@RequestHeader("Authorization") String token) {
+        // Authentication
+        String authenticatedUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+        User loggedinUser = userRepo.findByEmail(authenticatedUsername).orElseThrow();
+        int userId = loggedinUser.getId();
 
+        List<Bookmark> result = bookmarkRepository.findByBookmarkIdUserId(userId);
+        // returns HTTP status 200 whether user has bookmarks or not
         return result;
     }
 
