cc172769ae82cf1dfce3b4452da3913662ba97b4
Sondre Malerud <sondre_mal@hotmail.no>
2023-03-27 17:25:08 +0200

    Eksempel på testing

--- src/test/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/UserControllerTest.java
+++ src/test/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/UserControllerTest.java
@@ -3,37 +3,56 @@ package edu.ntnu.jakobkg.idatt2105projbackend.controller;
 import edu.ntnu.jakobkg.idatt2105projbackend.model.User;
 import edu.ntnu.jakobkg.idatt2105projbackend.repo.UserRepository;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.Assert.*;
-import org.junit.Test;
+import static org.mockito.Mockito.*;
 
+import org.junit.Test;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 @ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
 
     @Mock
     private UserRepository userRepository;
 
+    @InjectMocks
+    private UserController userController;
+
+    User testUser = new User("Testbruker", "Testmann", "test@test.no", "test", "test", 1234, "Testeby");
+
+
     @BeforeEach
     void setup() {
-        User user = new User("Testbruker", "Testmann", "test@test.no", "test", "test", 1234, "Testeby");
+        testUser = new User("Testbruker", "Testmann", "test@test.no", "test", "test", 1234, "Testeby");
     }
 
 
     @Test
     public void saveUserWorks() {
-        userRepository.save(new User("Testbruker", "Testmann", "test@test.no", "test", "test", 1234, "Testeby"));
-        Mockito.verify(userRepository).save(new User("Testbruker", "Testmann", "test@test.no", "test", "test", 1234, "Testeby"));
-        assertEquals(0, userRepository.count());
+        when(userRepository.save(testUser)).thenReturn(new User());
+        assertEquals(new User().getUsername(), userRepository.save(testUser).getUsername());
+    }
 
-        Mockito.when(userRepository.save(new User("Testbruker", "Testmann", "test@test.no", "test", "test", 1234, "Testeby"))).thenReturn(new User());
-        assertEquals(new User(), userRepository.save(new User("Testbruker", "Testmann", "test@test.no", "test", "test", 1234, "Testeby")));
+    @Test
+    public void getUserPositive() {
+        when(userRepository.findById(1)).thenReturn(Optional.of(testUser));
+        assertEquals(testUser, userController.getUser(1));
     }
 
 
