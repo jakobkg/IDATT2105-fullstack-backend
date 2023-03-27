9d26aaf8f4921f8cfb3bd1b556ffa335fa35385d
Jakob Grønhaug <jakob@gronha.ug>
2023-03-27 12:18:51 +0200

    Endret fra database-basert til filsystem-basert hilhåndtering

--- src/main/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/FileController.java
+++ src/main/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/FileController.java
@@ -1,16 +1,17 @@
 package edu.ntnu.jakobkg.idatt2105projbackend.controller;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.UUID;
 
-import org.hibernate.engine.jdbc.BlobProxy;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -23,27 +24,29 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import edu.ntnu.jakobkg.idatt2105projbackend.model.UploadedFile;
-import edu.ntnu.jakobkg.idatt2105projbackend.repo.FileRepository;
 import java.nio.file.Path;
 
 @Controller
 @RequestMapping(path = "/files")
 public class FileController {
-    @Autowired
-    FileRepository fileRepo;
 
-    String basePath = "${java.io.tmpdir}";
+    @Value("${filebucket.path}")
+    String basePath;
 
     @PostMapping("")
-    public ResponseEntity upload(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) {
+        if (!file.getContentType().equals(MediaType.IMAGE_JPEG_VALUE)) {
+            throw new ResponseStatusException(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+        }
         String filename = UUID.randomUUID() + ".jpg";
         Path path = Paths.get(basePath, filename);
+
         try {
             Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        
         String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path("/files/")
                 .path(filename)
@@ -53,14 +56,18 @@ public class FileController {
 
     @GetMapping("/{filename:.+}")
     public ResponseEntity download(@PathVariable String filename) {
-        UploadedFile file = fileRepo.findByFilename(filename);
-        try {
-            return ResponseEntity.ok()
-                    .contentType(MediaType.IMAGE_JPEG)
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
-                    .body(file.getContents().getBytes(1, (int) file.getContents().length()));
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        Path path = Paths.get(basePath + "/" + filename);
+	    Resource file = null;
+
+	    try {
+		    file = new UrlResource(path.toUri());
+	    } catch (MalformedURLException e) {
+		    e.printStackTrace();
+	    }
+        
+	    return ResponseEntity.ok()
+			.contentType(MediaType.IMAGE_JPEG)
+			.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+			.body(file);
     }
 }
