5816649fbbf02a717bb5a394f20dcca11b13ce61
Kasia <kasia.szlejter@gmail.com>
2023-03-27 13:02:31 +0200

    Finished item controller tests

--- src/test/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/ItemControllerTest.java
+++ src/test/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/ItemControllerTest.java
@@ -1,30 +1,37 @@
 package edu.ntnu.jakobkg.idatt2105projbackend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import edu.ntnu.jakobkg.idatt2105projbackend.model.AddItemRequest;
 import edu.ntnu.jakobkg.idatt2105projbackend.model.User;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.containsString;
-
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+
+/**
+ * Test class for ItemController
+ */
+
 @AutoConfigureMockMvc
+@SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ItemControllerTest {
     @Autowired
     private MockMvc mockMvc;
     public String token;
+
+    /**
+     * Test/run login before testing endpoints that require to be logged in
+     */
     @BeforeAll
     public void login() {
         User testUser = new User("Admin", "Adminsen", "admin@admin.no", "admin", "Admingata 1", 1101, "Adminby");
@@ -32,6 +39,10 @@ public class ItemControllerTest {
         this.token = TokenController.generateToken(testUser);
     }
 
+    /**
+     * AddItem - POST request to /item
+     * @throws Exception
+     */
     @Test
     public void addItem() throws Exception {
         AddItemRequest request = new AddItemRequest(
@@ -46,10 +57,73 @@ public class ItemControllerTest {
         );
 
         this.mockMvc.perform(post("/item")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(request))
-                .header("Authorization", "Bearer "+this.token))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(request))
+                        .header("Authorization", "Bearer "+this.token))
                 .andExpect(status().isCreated())
                 .andExpect(content().string(containsString("Hodetelefoner")));
     }
+
+    /**
+     * Update an item - PUT request to /item/id
+     * Expect: status code 200 and "1" on return which is item id for the updated item
+     * @throws Exception
+     */
+    @Test
+    public void updateItem() throws Exception {
+        AddItemRequest request = new AddItemRequest(
+                "Plante", "Veldig fin plante",
+                "63.404734992005245", "10.371198931159565", "0100 Sted",
+                "5000", 6,
+                "https://media.houseandgarden.co.uk/photos/618944690a583de660124d52/master/w_1600%2Cc_limit/1-house-29mar17-Nick-Pope_b.jpg"
+        );
+
+        //Check if returned status code is 200 and "1" is returned (item id)
+        this.mockMvc.perform(put("/item/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(request))
+                        .header("Authorization", "Bearer "+this.token))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("1")));
+    }
+
+
+    /**
+     * Delete an item - DELETE request to /item/id
+     * Expect: status code 200, nothing on return
+     * @throws Exception
+     */
+    @Test
+    public void deleteItem() throws Exception {
+        //Check if returned status code is 200
+        this.mockMvc.perform(delete("/item/1")
+                        .header("Authorization", "Bearer "+this.token))
+                .andExpect(status().isOk());
+    }
+
+    /**
+     * getItem - GET request to /item/id (no authorization required)
+     * Expect: status code 200 and item in return which contains "Plante" - this comes from startup/CreateItems
+     * @throws Exception
+     */
+    @Test
+    public void getItem() throws Exception {
+        this.mockMvc.perform(get("/item/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Plante")));
+    }
+
+    /**
+     * getItems - GET request to /item (no authorization required)
+     * Expect: status code 200 and
+     * @throws Exception
+     */
+    @Test
+    public void getItems() throws Exception {
+        this.mockMvc.perform(get("/item"))
+                .andExpect(status().isOk());
+                //.andExpect(jsonPath("$.*", hasSize(11))); -> we dont know the actual size of items
+    }
+
+
 }
