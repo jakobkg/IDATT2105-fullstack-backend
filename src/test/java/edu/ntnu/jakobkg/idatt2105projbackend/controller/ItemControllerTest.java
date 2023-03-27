e36e3337a87e86487772c88222aeb972db4d3639
Sondre Malerud <sondre_mal@hotmail.no>
2023-03-27 18:26:06 +0200

    Itemcontroller test

--- src/test/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/ItemControllerTest.java
+++ src/test/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/ItemControllerTest.java
@@ -10,8 +10,18 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.*;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -27,13 +37,13 @@ public class ItemControllerTest {
     Item testItem = new Item("title", "description", "date", "lat", "long", "location", "price", 1, "images", 1);
 
     @Test
-    public void addItemWorks() {
+    public void findByCategoryWorks() {
+        ArrayList<Item> arrayList = new ArrayList<>();
+        arrayList.add(testItem);
+        itemRepository.save(testItem);
+        when(itemRepository.findByCategoryId(1, PageRequest.of(0, 24))).thenReturn(arrayList);
 
-
-
+        assertEquals(itemController.getMultiple(0, 1, -1), arrayList);
     }
-
-
-
 
 }
