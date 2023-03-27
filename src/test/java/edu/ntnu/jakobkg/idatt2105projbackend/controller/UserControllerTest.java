34519a74c2efc95c07363b55eb2de8a76bf790b2
Sondre Malerud <sondre_mal@hotmail.no>
2023-03-27 16:17:14 +0200

    Eksempel på testing

--- src/test/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/UserControllerTest.java
+++ src/test/java/edu/ntnu/jakobkg/idatt2105projbackend/controller/UserControllerTest.java
@@ -1,11 +1,40 @@
 package edu.ntnu.jakobkg.idatt2105projbackend.controller;
 
+import edu.ntnu.jakobkg.idatt2105projbackend.model.User;
+import edu.ntnu.jakobkg.idatt2105projbackend.repo.UserRepository;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-@AutoConfigureMockMvc
-@SpringBootTest
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setup() {
+        User user = new User("Testbruker", "Testmann", "test@test.no", "test", "test", 1234, "Testeby");
+    }
+
+
+    @Test
+    public void saveUserWorks() {
+        userRepository.save(new User("Testbruker", "Testmann", "test@test.no", "test", "test", 1234, "Testeby"));
+        Mockito.verify(userRepository).save(new User("Testbruker", "Testmann", "test@test.no", "test", "test", 1234, "Testeby"));
+        assertEquals(0, userRepository.count());
+
+        Mockito.when(userRepository.save(new User("Testbruker", "Testmann", "test@test.no", "test", "test", 1234, "Testeby"))).thenReturn(new User());
+        assertEquals(new User(), userRepository.save(new User("Testbruker", "Testmann", "test@test.no", "test", "test", 1234, "Testeby")));
+    }
+
+
 }
